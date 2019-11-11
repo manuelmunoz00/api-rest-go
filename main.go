6112b3137857d5e052793b69a297ab57d621ad07
Manuel Muñoz <mmunoz@digital.gob.cl>
2019-11-11 15:13:31 -0300

    se agrega libreria godotenv

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+	s3Bucket := os.Getenv("S3_BUCKET")
+	//secretKey := os.Getenv("SECRET_KEY")
+
+	fmt.Fprintf(w, s3Bucket)
 }
 
 func main() {
