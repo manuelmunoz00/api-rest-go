e05d2fde94a92a6a0a69ff9fac1580fab20e9035
Manuel Muñoz <mmunoz@digital.gob.cl>
2019-11-13 17:27:50 -0300

    se modifica readme y modificaciones a struct cliente

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 cp .env.example .env
 
-go get
+go get -v -u github.com/go-chi/chi
+go get -v -u github.com/jinzhu/gorm
+go get -v -u github.com/joho/godotenv
--- models/cliente.go
+++ models/cliente.go
@@ -2,9 +2,19 @@ package models
 
 // Cliente struct
 type Cliente struct {
-	ID        int    `json:"id"`
+	ID        uint   `json:"id"`
 	Rut       string `json:"rut"`
 	Nombres   string `json:"nombres"`
 	Apellidos string `json:"apellidos"`
 	Correo    string `json:"correo"`
+}
+
+// NewCliente función para crear nuevo cliente
+func NewCliente(c Cliente) {
+
+}
+
+// GetCliente función para obtener cliente
+func GetCliente() {
+
 }
