f05b5ba2614f2fa826177ff2ba4858bf2b00f554
Manuel Muñoz <mmunoz@digital.gob.cl>
2019-11-15 09:58:10 -0300

    se elimina import a package no existente

--- models/oferta.go
+++ models/oferta.go
@@ -2,14 +2,12 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	otro "github.com/manuelmunoz00/api-rest-go/otros"
 )
 
 // Oferta struct
 type Oferta struct {
 	gorm.Model
-	ID            uint `gorm:"PRIMARY_KEY;AUTO_INCREMENT"`
-	Nombre        string
-	Precio        int
-	ListadoMetros []otro.Metro
+	ID     uint `gorm:"PRIMARY_KEY;AUTO_INCREMENT"`
+	Nombre string
+	Precio int
 }
